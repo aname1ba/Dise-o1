3f65536b4952b15e2d5bf7366e259bd4241512c6
aname1ba <117988405+aname1ba@users.noreply.github.com>
2023-01-12 00:16:11 -0500

    Update diseñog1.py

--- diseñog1.py
+++ diseñog1.py
@@ -5,3 +5,4 @@ import pandas as pd
 import numpy as np
 
 st.title('EQUIPO 1')
+st.write('Integrantes: Luciana Aychasi, Jhonatan Guevara, Anamelba Fernández, Yassmin Parillo, Natalie Quiñonez y Aracely Taipe')
